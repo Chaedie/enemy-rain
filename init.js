c630128343a8355ea7ce0df151216b9d2e8f9155
Chaedie <chaedong.im.dev@gmail.com>
2022-07-20 22:19:04 +0900

    feat : Hero 좌우 이동 구현

--- init.js
+++ init.js
@@ -1,11 +1,13 @@
-const hero = document.querySelector('.hero__img');
+const hero = document.querySelector('#hero__img');
+const MOVE_POS = 15;
+hero.style.left = window.getComputedStyle(hero).left;
 
 window.addEventListener('keydown', (e) => {
   if (e.code === 'ArrowLeft') {
-    hero.className = 'hero__img hero__left';
+    hero.className = 'hero__left';
+    hero.style.left = parseInt(hero.style.left) - MOVE_POS + 'px';
   } else if (e.code === 'ArrowRight') {
-    hero.className = 'hero__img hero__right';
+    hero.className = 'hero__right';
+    hero.style.left = parseInt(hero.style.left) + MOVE_POS + 'px';
   }
-
-  return console.log(e.code);
 });
