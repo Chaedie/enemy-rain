b90d2817c55e843ccff7595e0a10a81aa1766873
Chaedie <chaedong.im.dev@gmail.com>
2022-07-21 16:49:03 +0900

    feat : 여러 Enemy 생성, 내려오는 함수 구현

--- init.js
+++ init.js
@@ -1,28 +1,27 @@
 const hero = document.querySelector('#hero__img');
-const MOVE_POS = 15;
 const bg = document.querySelector('#bg');
+const enemy = document.querySelector('.enemy__img');
 
-hero.style.left = window.getComputedStyle(hero).left;
+const MOVE_POS = 15;
 
 window.addEventListener('keydown', (e) => {
+  hero.style.left = window.getComputedStyle(hero).left;
   if (e.code === 'ArrowLeft') {
     hero.className = 'hero__left';
     hero.style.left = parseInt(hero.style.left) - MOVE_POS + 'px';
-  } else if (e.code === 'ArrowRight') {
+  }
+
+  if (e.code === 'ArrowRight') {
     hero.className = 'hero__right';
     hero.style.left = parseInt(hero.style.left) + MOVE_POS + 'px';
   }
 });
 
-const enemy = document.querySelector('.enemy__img');
-const enemies = document.querySelectorAll('.enemy__img');
-const enemyInstance = document.createElement('div');
-
 const enemyDown = () => {
   enemy.style.top = window.getComputedStyle(enemy).top;
   enemy.style.left = window.getComputedStyle(enemy).left;
   hero.style.left = window.getComputedStyle(hero).left;
-  //
+
   const heroPositionLeft = parseInt(hero.style.left);
   const enemyPositionLeft = parseInt(enemy.style.left);
   const enemyPositionTop = parseInt(enemy.style.top);
@@ -35,16 +34,49 @@ const enemyDown = () => {
     } else {
       enemy.style.display = 'none';
     }
-    // enemy.style.top = enemyPositionTop + MOVE_POS + 'px';
-    // if (enemyPositionTop > 510) {
-    // }
   }
 };
 
 const enemyCreate = () => {
+  let id = ids.length;
+  const enemyInstance = document.createElement('div');
+  enemyInstance.id = id;
+  enemyInstance.style.left = Number(Math.random() * 550) + 'px';
   enemyInstance.className = 'enemy__img';
   bg.appendChild(enemyInstance);
+  ids.push(id);
+  console.log(ids);
+  console.log(ids.length);
+  setInterval(newEnemyDown, 100, id);
 };
 
-setInterval(enemyDown, 50);
-// setInterval(enemyCreate, 200);
+const newEnemyDown = (id) => {
+  const newEnemy = document.getElementById(`${id}`);
+
+  console.log(`#${id.toString()}`);
+  console.log(id);
+
+  newEnemy.style.top = window.getComputedStyle(newEnemy).top;
+  newEnemy.style.left = window.getComputedStyle(newEnemy).left;
+  hero.style.left = window.getComputedStyle(hero).left;
+
+  const heroPositionLeft = parseInt(hero.style.left);
+  const enemyPositionLeft = parseInt(newEnemy.style.left);
+  const enemyPositionTop = parseInt(newEnemy.style.top);
+
+  if (enemyPositionTop < 480) {
+    newEnemy.style.top = enemyPositionTop + MOVE_POS + 'px';
+  } else {
+    if (heroPositionLeft - 54 < enemyPositionLeft && enemyPositionLeft < heroPositionLeft + 54) {
+      return (newEnemy.className = 'enemy__img enemy__die');
+    } else {
+      newEnemy.style.display = 'none';
+    }
+  }
+};
+
+// setInterval(enemyDown, 50);
+// setInterval(enemiesDown, 50);
+const ids = [0];
+enemyCreate();
+setInterval(enemyCreate, 1000);
