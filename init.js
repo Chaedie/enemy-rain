3984c3119dcd95e892a4e9b3d708e34d9963ea4f
Chaedie <chaedong.im.dev@gmail.com>
2022-07-21 17:55:17 +0900

    refactor : offset 사용하여 불필요한 위치 계산 부분 삭제, 변수 및 함수명 리팩터링

--- init.js
+++ init.js
@@ -1,63 +1,54 @@
 const hero = document.querySelector('#hero__img');
 const bg = document.querySelector('#bg');
-const enemy = document.querySelector('.enemy__img');
 
 const MOVE_POS = 15;
 let gameScore = 0;
 
 window.addEventListener('keydown', (e) => {
-  hero.style.left = window.getComputedStyle(hero).left;
   if (e.code === 'ArrowLeft') {
     hero.className = 'hero__left';
-    hero.style.left = parseInt(hero.style.left) - MOVE_POS + 'px';
+    hero.style.left = `${hero.offsetLeft - MOVE_POS}px`;
   }
 
   if (e.code === 'ArrowRight') {
     hero.className = 'hero__right';
-    hero.style.left = parseInt(hero.style.left) + MOVE_POS + 'px';
+    hero.style.left = `${hero.offsetLeft + MOVE_POS}px`;
   }
 });
 
-const enemyCreate = () => {
+function enemyCreate() {
   let id = ids.length;
-  const enemyInstance = document.createElement('div');
-  enemyInstance.id = id;
-  enemyInstance.style.left = Number(Math.random() * 550) + 'px';
-  enemyInstance.className = 'enemy__img';
-  bg.appendChild(enemyInstance);
+  const enemy = document.createElement('div');
+  enemy.id = id;
+  enemy.style.left = Number(Math.random() * 550) + 'px';
+  enemy.className = 'enemy__img';
+  bg.appendChild(enemy);
   ids.push(id);
-  setInterval(newEnemyDown, 100, id);
-};
+  setInterval(enemyDown, 100, id);
+}
 
-const newEnemyDown = (id) => {
-  const newEnemy = document.getElementById(`${id}`);
+function enemyDown(id) {
+  const enemy = document.getElementById(`${id}`);
 
-  newEnemy.style.top = window.getComputedStyle(newEnemy).top;
-  newEnemy.style.left = window.getComputedStyle(newEnemy).left;
-  hero.style.left = window.getComputedStyle(hero).left;
-
-  const heroPositionLeft = parseInt(hero.style.left);
-  const enemyPositionLeft = parseInt(newEnemy.style.left);
-  const enemyPositionTop = parseInt(newEnemy.style.top);
-
-  if (enemyPositionTop < 480) {
-    newEnemy.style.top = enemyPositionTop + MOVE_POS + 'px';
+  if (enemy.offsetTop < 480) {
+    enemy.style.top = `${enemy.offsetTop + MOVE_POS}px`;
   } else {
-    if (heroPositionLeft - 54 < enemyPositionLeft && enemyPositionLeft < heroPositionLeft + 54) {
-      gameScore = gameScore + 1;
-      console.log('들어옴');
-      setScore();
-      return (newEnemy.className = 'enemy__img enemy__die');
+    if (hero.offsetLeft - 54 < enemy.offsetLeft && enemy.offsetLeft < hero.offsetLeft + 54) {
+      enemy.className = 'enemy__img enemy__die';
+      setTimeout(() => {
+        gameScore++;
+        setScore();
+        bg.removeChild(enemy);
+      }, 100);
     } else {
-      newEnemy.style.display = 'none';
+      enemy.style.display = 'none';
     }
   }
-};
+}
 
 const score = document.querySelector('.score__span');
 const setScore = () => {
   score.innerHTML = gameScore;
-  console.log(gameScore);
 };
 
 const ids = [0];
